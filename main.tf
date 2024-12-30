b6c9c93a6ce937426e3cfc4675d5df4e1b66d925
Nathan Bills <nathan@nerdmeeting.com>
2024-12-29 17:13:59 -0700

    Use the new cors_rules variable.
    These example rules limit PUT and POST requests to an example domain, and permit GET requests from anywhere.

--- main.tf
+++ main.tf
@@ -22,6 +22,20 @@ module "website_s3_bucket" {
     terraform_managed = true
     www_path = "${path.root}/www"
   }
+  
+  cors_rules = [
+    {
+      allowed_headers = ["*"],
+      allowed_methods = ["PUT", "POST"],
+      allowed_origins = ["https://test.example.com"],
+      expose_headers = ["ETag"],
+      max_age_seconds = 3000
+    },
+    {
+      allowed_methods = ["GET"],
+      allowed_origins = ["*"]
+    }
+  ]
 
   tags = {
     terraform     = "true"
